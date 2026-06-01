4526d8ef63d050b3af95260c0d1c26ebdb9aa296
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2026-06-01 07:31:27 +0000

    feat: ready for cloud deployment

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,7 @@ const db = mysql.createConnection({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   port:     process.env.DB_PORT || 3306,
+  ssl: { rejectUnauthorized: false }
 });
 
 db.connect((err) => {
